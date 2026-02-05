2e134d3fcbed6febe5b39536c9c6050b32d3d4f6
swapnil.singh <swapnil.singh.mst24@itbhu.ac.in>
2026-02-05 12:07:31 +0530

    Done

---
+++ Colour Picker/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.body;
+
+buttons.forEach(function(button){
+    button.addEventListener("click",function(e)
+{
+    switch(e.target.id)
+    {
+        case "red": body.style.backgroundColor = "red";
+        break;
+        case "green":body.style.backgroundColor ="green";
+        break;
+        case "blue": body.style.backgroundColor = "blue";
+        break;
+        case "yellow": body.style.backgroundColor = "yellow";
+        break;
+        case "orange": body.style.backgroundColor = "orange";
+        break;
+        case "purple": body.style.backgroundColor = "purple";
+        break;
+    }
+})
+})
